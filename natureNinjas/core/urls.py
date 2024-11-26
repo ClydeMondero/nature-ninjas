9d5fc4ffa1c3cd6317ac1d53ee42bad09401ef30
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-11-27 01:05:36 +0800

    feat: added models and  home page

--- natureNinjas/core/urls.py
+++ natureNinjas/core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('about/', views.about, name='about'),  # About page
     path('activities/', views.activities, name='activities'),  # Activities page
-    path('articles/', views.articles, name='articles'),  # Articles page
+    path('articles/', views.article_list, name='articles'),
+    path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
     path('multimedia/', views.multimedia, name='multimedia'),  # Multimedia page
 ]
