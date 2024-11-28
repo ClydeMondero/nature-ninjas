d3ec916f56437a7b395a53adf40cfd1cae2e8a80
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-11-28 22:23:49 +0800

    feat: added multimedia and activity

--- natureNinjas/core/admin.py
+++ natureNinjas/core/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Multimedia, Activity
+
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title', 'slug', 'created_at', 'activity', 'multimedia')
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Multimedia)
+class MultimediaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
--- natureNinjas/core/views.py
+++ natureNinjas/core/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Article
+from .models import Article, Multimedia, Activity
 
 # Create your views here.
 def home(request):
     articles = Article.objects.all().order_by('-created_at')
-    return render(request, 'home.html', {'articles': articles})
+    multimedia = Multimedia.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'articles': articles, 'multimedia': multimedia})
 
 def about(request):
     return render(request, 'about.html')
-
-def activities(request):
-    return render(request, 'activities.html')
 
 
 def article_list(request):
@@ -19,13 +17,44 @@ def article_list(request):
     return render(request, 'articles/list.html', {'articles': articles})
 
 def article_detail(request, slug):
+    # Fetch the main article
     article = get_object_or_404(Article, slug=slug)
-    return render(request, 'articles/detail.html', {'article': article})
+    
+    # Fetch related articles based on the latest articles, excluding the current one
+    related_articles = Article.objects.exclude(id=article.id).order_by('-created_at')[:3]
+    
+    return render(request, 'articles/detail.html', {
+        'article': article,
+        'related_articles': related_articles
+    })
 
 
+# Multimedia Views
+def multimedia_list(request):
+    multimedia = Multimedia.objects.all().order_by('-created_at')
+    return render(request, 'multimedia/list.html', {'multimedia': multimedia})
 
-def multimedia(request):
-    return render(request, 'multimedia.html')
+def multimedia_detail(request, id):
+    media = get_object_or_404(Multimedia, id=id)
+    # Fetch related multimedia, excluding the current one
+    related_multimedia = Multimedia.objects.exclude(id=media.id).order_by('-created_at')[:3]
+    return render(request, 'multimedia/detail.html', {
+        'multimedia': media,
+        'related_multimedia': related_multimedia
+    })
+
+# Activity Views
+def activity_list(request):
+    activities = Activity.objects.all().order_by('-created_at')
+    return render(request, 'activities/list.html', {'activities': activities})
+
+def activity_detail(request, id):
+    activity = get_object_or_404(Activity, id=id)
+    # Fetch articles associated with this activity
+    related_articles = activity.articles.all()
+    return render(request, 'activities/detail.html', {
+        'activity': activity,
+    })
 
 def contact(request):
-    return render(request, 'contact.html')  
+    return render(request, 'contact.html')
